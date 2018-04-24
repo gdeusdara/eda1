3e25f443d3181ca40ae7e26b61c1f29ec0b38934
Guilherme Deusdará <guibanci@gmail.com>
2018-04-24 09:10:22 -0300

    deixando sorteio sem repeticao
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>

--- projeto2/main.c
+++ projeto2/main.c
@@ -8,16 +8,21 @@ int main(int argc, char const *argv[]) {
    char *extension = ".txt";
    char *type = "asphalt_";
    char fileName[strlen(type)+strlen(extension)+3];
-   int number;
+   int number[FILES];
 
-   srand(time(0));
+   srand(time(NULL));
 
-   for (size_t i = 0; i <= FILES; i++) {
+   for (size_t i = 0; i < FILES; i++) {
      do{
-       number = rand()%51;
-     }while(number == 0);
+       number[i] = rand()%51;
+       for (size_t j = 0; j < i; j++) {
+         if (number[j] == number[i]) {
+           number[i] = 0;
+         }
+       }
+     }while(number[i] == 0);
 
-     snprintf( fileName, sizeof( fileName ), "%s%.2d%s", type, number, extension );
+     snprintf( fileName, sizeof( fileName ), "%s%.2d%s", type, number[i], extension );
      printf("%s",fileName);
    }
   return 0;
