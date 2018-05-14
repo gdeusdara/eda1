44b5156f62cb11160dba17805a53ec4a2ea7569d
Guilherme Deusdará <guibanci@gmail.com>
2018-05-14 16:54:04 -0300

    projeto3 - deixando a lista encadeada em ordem alfabetica

--- projeto3/main.c
+++ projeto3/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 #define FILE_NAME "contatos.txt"
 #define MAX_STRING 100
@@ -19,52 +21,144 @@ typedef struct list
 {
     struct list *previous;
     Contact info;
-    struct list *after;
+    struct list *next;
 } List;
 
-void new_register();
+void init_element(List *element, Contact new_register);
+void init_list(List *list);
+void new_register(List *list);
 void remove_register(char *name);
 void show_register(char *name);
-void show_all();
+void show_all(List *list);
 
 int main ()
 {
-    new_register();
-    return 0;
+    List *list = (List *) malloc(sizeof(List));
+    if(!list)
+    {
+		    printf("Sem memoria disponivel!\n");
+		    exit(1);
+	  }
+    else
+    {
+        init_list(list);
+
+        new_register(list);
+        printf("%s\n", list->next->info.name  );
+        new_register(list);
+        printf("%s\n", list->next->info.name);
+        new_register(list);
+        show_all(list);
+        return 0;
+    }
 }
 
-void new_register()
+void init_element(List *element, Contact new_register)
 {
-    Contact new;
+    element->previous = NULL;
+    element->next = NULL;
+    element->info = new_register;
+
+}
+
+void init_list(List *list)
+{
+  list->previous = NULL;
+  list->next = NULL;
+}
+
+void new_register(List *list)
+{
+    Contact new_register;
     FILE *file;
+    List *new = (List *) malloc(sizeof(List));
 
-    new.name = (char *) malloc(sizeof(char *));
+    new_register.name = (char *) malloc(sizeof(char *));
     printf ("Digite o nome: ");
-    scanf("%[^\n]%*c", new.name);
+    scanf("%[^\n]%*c", new_register.name);
 
-    new.tel = (char *) malloc(sizeof(char *));
+    new_register.tel = (char *) malloc(sizeof(char *));
     printf("Digite o telefone: ");
-    scanf("%[^\n]%*c", new.tel);
+    scanf("%[^\n]%*c", new_register.tel);
 
-    new.adress = (char *) malloc(sizeof(char *));
+    new_register.adress = (char *) malloc(sizeof(char *));
     printf("Digite o Endereço: ");
-    scanf("%[^\n]%*c", new.adress);
+    scanf("%[^\n]%*c", new_register.adress);
 
 
     printf("Digite o CEP: ");
-    scanf("%d%*c", &new.cep);
+    scanf("%d%*c", &new_register.cep);
 
-    new.birthday = (char *) malloc(sizeof(char *));
+    new_register.birthday = (char *) malloc(sizeof(char *));
     printf("Digite a data de nascimento: ");
-    scanf("%[^\n]%*c", new.birthday);
+    scanf("%[^\n]%*c", new_register.birthday);
 
-
+    init_element(new, new_register);
+    printf("%s\n", new->info.name );
     file = fopen(FILE_NAME, "a");
     if (file == NULL)
         exit(1);
 
-    if(!fprintf(file, "%s\n%s\n%s\n%d\n%s\n$\n", new.name, new.tel, new.adress, new.cep, new.birthday))
+    if(list->next == NULL)
+    {
+        list->next = new;
+    } else
+    {
+        List *element = list->next;
+        printf("%s\n", element->info.name);
+
+        bool finish = false;
+        do
+        {
+            if(strcmp(new->info.name, element->info.name) < 0)
+            {
+                List *previous = list->next;
+                do {
+                  /* code */
+                if (previous == element) {
+                  printf("Estou no primeiro elemento\n");
+                  list->next = new;
+                  new->next = element;
+                  element->previous = new;
+                  finish = true;
+                }
+                previous = previous->next;
+              } while(!finish);
+
+            }
+            else if (element->next == NULL) {
+                printf("Estou no segundo if\n");
+                element->next = new;
+                new->previous = element;
+                finish = true;
+            }
+            element = element->next;
+        } while (!finish || element == NULL);
+
+    }
+    printf("AGORA VOU GRAVAR NO ARQUIVO\n");
+
+
+    if(!fprintf(file, "%s\n%s\n%s\n%d\n%s\n$\n", new_register.name, new_register.tel, new_register.adress, new_register.cep, new_register.birthday))
       printf("Erro ao gravar no arquivo\n");
     if(fclose(file))
       printf("Erro ao fechar Arquivo\n");
+}
+
+void show_all(List *list)
+{
+    if(list->next == NULL){
+      printf("Lista vazia!\n\n");
+      return ;
+    }
+
+    List *element;
+    element = list->next;
+    printf("Lista:\n");
+    while( element != NULL){
+        printf("Nome: %s\nTelefone: %s\nEndereço: %s\nCEP: %d\nData de Nascimento: %s\n\n", element->info.name, element->info.tel, element->info.adress, element->info.cep, element->info.birthday);
+        element = element->next;
+    }
+
+printf("\n\n");
 }
