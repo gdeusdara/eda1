0007d122d3676176bf88b3cd80b6d9379270335a
Guilherme Deusdará <guibanci@gmail.com>
2018-05-16 22:45:29 -0300

    projeto3 - menu e função de apagar registro adicionados

--- projeto3/main.c
+++ projeto3/main.c
@@ -30,12 +30,15 @@ void create_list(List *list, Contact new_register);
 void init_element(List *element, Contact new_register);
 void init_list(List *list);
 void new_register(List *list);
-void remove_register(char *name);
+void remove_register(List *list);
 void show_register(char *name);
 void show_all(List *list);
+void free_all(List *list);
+void save_to_file(List *list);
 
 int main ()
 {
+    int option;
     List *list = (List *) malloc(sizeof(List));
     if(!list)
     {
@@ -44,9 +47,45 @@ int main ()
 	  }
     else
     {
-        init_list(list);
-        show_all(list);
-        return 0;
+      init_list(list);
+      do{
+        printf("\n\n\n");
+        printf("\t\t\t============MENU============\t\t\n");
+        printf("\n\n");
+        printf("\t\t\t[1] Inserir Novo Registro\n");
+        printf("\t\t\t[2] Remover Registro\n");
+        printf("\t\t\t[3] Busca de Registro por Nome\n");
+        printf("\t\t\t[4] Lista de Registro\n");
+        printf("\t\t\t[5] SAIR\n\n");
+
+        printf("\t\t\tDigite uma opção: ");
+        scanf("%d", &option);
+        printf("\n");
+
+        switch (option) {
+          case 1:
+            new_register(list);
+            break;
+          case 2:
+            remove_register(list);
+            //remove_register(name);
+            break;
+          case 3:
+            printf ("CASE 3\n");
+            //show_register(name);
+            break;
+          case 4:
+            show_all(list);
+            break;
+          case 5:
+            free_all(list);
+            return 0;
+          default:
+            printf ("\tOPÇÃO INVÁLIDA, DIGITE NOVAMENTE!\n\n");
+        }
+      }while(option!=5);
+
+      return 0;
     }
 }
 // =========== FUNCTIONS ==========
@@ -88,7 +127,7 @@ void init_list(List *list)
     file = fopen(FILE_NAME, "r");
     if (file == NULL)
     {
-      printf("nao consegui LEr\n");
+      printf("Não consegui ler\n");
       list->next = NULL;
     } else
     {
@@ -127,16 +166,16 @@ void new_register(List *list)
 
     new_register.name = (char *) malloc(sizeof(char *));
     printf ("Digite o nome: ");
-    scanf("%[^\n]%*c", new_register.name);
+    scanf(" %[^\n]%*c", new_register.name);
 
 
     new_register.tel = (char *) malloc(sizeof(char *));
     printf("Digite o telefone: ");
-    scanf("%[^\n]%*c", new_register.tel);
+    scanf(" %[^\n]%*c", new_register.tel);
 
     new_register.adress = (char *) malloc(sizeof(char *));
     printf("Digite o Endereço: ");
-    scanf("%[^\n]%*c", new_register.adress);
+    scanf(" %[^\n]%*c", new_register.adress);
 
 
     printf("Digite o CEP: ");
@@ -186,22 +225,42 @@ void new_register(List *list)
             }
         } while (!finish || element == NULL);
 
+        save_to_file(list);
     }
 
-    file = fopen(FILE_NAME, "w");
-    if (file == NULL)
-        exit(1);
+}
 
-    List *element = list->next;
-    while (element != NULL)
-    {
-        if(!fprintf(file, "%s\n%s\n%s\n%d\n%s\n$\n", element->info.name, element->info.tel, element->info.adress, element->info.cep, element->info.birthday))
-            printf("Erro ao gravar no arquivo\n");
-        element = element->next;
-    }
 
-    if(fclose(file))
-      printf("Erro ao fechar Arquivo\n");
+void remove_register(List *list)
+{
+  char *name = (char *) malloc(sizeof(char *));
+  bool find = false;
+  printf("\nQual o nome do registro que deseja remover? ");
+  scanf(" %[^\n]%*c", name);
+
+  List *element = list->next;
+  while( element != NULL)
+  {
+      if(strcasecmp(name, element->info.name) == 0)
+      {
+            if (element->previous != NULL)
+            {
+                element->previous->next = element->next;
+            }
+            if (element->next != NULL)
+            {
+                element->next->previous = element->previous;
+            }
+            free_element(element);
+            printf("Registro apagado com sucesso\n");
+            find = true;
+      }
+      element = element->next;
+  }
+  if (!find) {
+    printf("Registro não encontrado!\n");
+  }
+  save_to_file(list);
 }
 
 void show_all(List *list)
@@ -213,11 +272,42 @@ void show_all(List *list)
 
     List *element;
     element = list->next;
-    printf("\nLILSTA:\n\n");
+    printf("\nLISTA:\n\n");
     while( element != NULL){
         printf("Nome: %s\nTelefone: %s\nEndereço: %s\nCEP: %d\nData de Nascimento: %s\n\n", element->info.name, element->info.tel, element->info.adress, element->info.cep, element->info.birthday);
         element = element->next;
     }
 
 printf("\n\n");
+}
+
+void free_all(List *list)
+{
+    List *element = list->next;
+    free(list);
+    while( element != NULL)
+    {
+        List *next = element->next;
+        free_element(element);
+        element = next;
+    }
+}
+
+void save_to_file(List * list)
+{
+
+  FILE *file = fopen(FILE_NAME, "w");
+  if (file == NULL)
+      exit(1);
+
+  List *element = list->next;
+  while (element != NULL)
+  {
+      if(!fprintf(file, "%s\n%s\n%s\n%d\n%s\n$\n", element->info.name, element->info.tel, element->info.adress, element->info.cep, element->info.birthday))
+          printf("Erro ao gravar no arquivo\n");
+      element = element->next;
+  }
+
+  if(fclose(file))
+    printf("Erro ao fechar Arquivo\n");
 }
