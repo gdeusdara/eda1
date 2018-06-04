87ccd7def4d36fb177b96284b13bd2d9728eff2e
Guilherme Deusdará <guibanci@gmail.com>
2018-06-04 16:32:26 -0300

    projeto4 - fazendo simulação sem quebrar

--- projeto4/main.c
+++ projeto4/main.c
@@ -156,11 +156,12 @@ Fila iniciar_avioes(int * NVoos, int *NAproximacoes, int *NDecolagens){
 //Verifica se não tem alguma prioridade 0 e, caso tenha, passa para o inicio da fila
 //Verifica se tem 3 ou mais com prioridade 0.
 bool verifica_emergencia(Fila *voos){
+  int emergencia = false;
+  if (voos->inicio != NULL) {
+      /* code */
     Lista *voo = voos->inicio->prox;
     Lista *voo_anterior = voos->inicio;
-    int emergencia = false;
-
-    while (voo != NULL) {
+    while (voo != NULL && voo_anterior != NULL) {
       if (voo->info.status == 'A') {
         if (voo->info.prioridade == 0) {
           voo_anterior->prox = voo->prox;
@@ -168,16 +169,20 @@ bool verifica_emergencia(Fila *voos){
           voos->inicio = voo;
         }
       }
-      voo_anterior = voo;
-      voo = voo->prox;
+      voo_anterior = voo_anterior->prox;
+      if (voo_anterior != NULL) {
+        voo = voo_anterior->prox;
+      }
     }
-
     //Agora vamos verificar se há emergencia
-    voo = voos->inicio->prox->prox;    //colocando ponteiro para o terceiro da fila
-
-    if (voo->info.prioridade == 0) {      //verificando se o terceiro da fila é 0
-        emergencia = true;
-        printf("ALERTA GERAL DE DESVIO DE AERONAVE\n");                //Se só o terceiro for 0, ele apenas abre a pista apenas de decolagem,                   //Se se o quarto também for 0, ele avisará que algum/alguns avião(ões) irá(ão) cair;
+    if (voos->inicio->prox != NULL)
+      if (voos->inicio->prox->prox != NULL) {
+        voo = voos->inicio->prox->prox;    //colocando ponteiro para o terceiro da fila
+        if (voo->info.prioridade == 0) {      //verificando se o terceiro da fila é 0
+            emergencia = true;
+            printf("ALERTA GERAL DE DESVIO DE AERONAVE\n");                //Se só o terceiro for 0, ele apenas abre a pista apenas de decolagem,                   //Se se o quarto também for 0, ele avisará que algum/alguns avião(ões) irá(ão) cair;
+        }
+      }
     }
     return emergencia;
 }
@@ -189,10 +194,10 @@ void simular(Fila *voos){
   bool emergencia;
   bool pista1 = true, pista2 = true, pista3 = true;
   int tempo1, tempo2, tempo3;
-
   while (voos->inicio != NULL) {    //iniciando simulação
-
-    emergencia = verifica_emergencia(voos);     //Coloca as prioridades 0 na frente e avisa se tem emergencia
+    if (!tempo) {
+      emergencia = verifica_emergencia(voos);     //Coloca as prioridades 0 na frente e avisa se tem emergencia
+    }
 
     //Se qualquer pista estiver liberada, ele entrará no if da pista para o pedido ser aceito;
     if (pista1) {
@@ -204,7 +209,7 @@ void simular(Fila *voos){
       pista1 = false;                                          //Se for solicitação para decolagem, ele poderá pedir para as 3 pistas
     }
 
-    if (pista2) {
+    if (pista2 && voos->inicio != NULL) {
       if (voos->inicio->info.status == 'D')
         tempo2 = 2*UNTEMPO;
       else
@@ -216,7 +221,7 @@ void simular(Fila *voos){
       aviao_cai(voos);
     }
 
-    if (pista3) {
+    if (pista3 && voos->inicio != NULL) {
       if (emergencia){      //a pista 3 abre para pouso em caso de emergencia
         tempo3 = 3*UNTEMPO;
         pedido_aceito(voos, tempo, 3);
@@ -251,8 +256,11 @@ void simular(Fila *voos){
     }
 
     //Esse if é para diminuir o tanque de tds os avioes a cada 10*UNTEMPO
-    if (tempo%(10*UNTEMPO) == 0) {
+    if (tempo%(10*UNTEMPO) == 0 && tempo != 0) {
+      printf("DIMINUI TANQUE\n");
       diminui_tanque(voos);
+      printf("Antes de entrar o EMERGENCIA\n");
+      emergencia = verifica_emergencia(voos);     //Coloca as prioridades 0 na frente e avisa se tem emergencia
     }
 
     //tempo passa de 5 em 5 minutos a cada loop
@@ -286,4 +294,5 @@ void aviao_cai(Fila *voos){
   Lista *queda = voos->inicio;
   voos->inicio = voos->inicio->prox;
   free(queda);
+  printf("ALERTA CRÍTICO, AERONAVE IRÁ CAIR\n");
 }
